a82d6ddf711597adab3412dc2d9592038d32279e
GuiTavs7 <7tavs7@gmail.com>
2026-09-15 13:48:14 -0300

    add usuarios functions

--- routes/usuario.js
+++ routes/usuario.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const mongoose = require('mongoose');
 require('../models/Usuario');
 const Usuario = mongoose.model('usuarios');
+const Categoria = mongoose.model('Categorias');
 const bcrypt = require('bcryptjs');
 const passport = require('passport');
 
@@ -94,6 +95,15 @@ router.get('/logout', (req, res) => {
         }
         req.flash("success_msg", "Deslogado com sucesso!");
         res.redirect("/");
+    });
+});
+
+router.get('/postagens/add', (req, res) => {
+    Categoria.find().lean().then((categorias) => {
+        res.render('usuarios/addpostagem', {categorias: categorias});
+    }).catch((err) => {
+        req.flash("error_msg", "Houve um erro ao carregar as categorias");
+        res.redirect('/admin');
     });
 });
 
